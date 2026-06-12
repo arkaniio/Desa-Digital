41e9cf4489ece7754b13f54cff6c7f9d1ba99a2c
arkaniio <laluahmadarkani@gmail.com>
2026-06-12 20:45:50 +0800

    Add: adding the complete controller for dashboard service

--- src/dashboard/dashboard.controller.ts
+++ src/dashboard/dashboard.controller.ts
@@ -16,4 +16,45 @@ export class DashboardController {
         return this.dashboardService.getDashboardData(userId, role)
     }
 
+    @Get("super_admin")
+    @UseGuards(JwtAuthGuard)
+    async superAdminDashboard(
+        @CurrentUser("userId") userId: number
+    ) {
+        return this.dashboardService.superAdminDashboard(userId)
+    }
+
+    @Get("warga")
+    @UseGuards(JwtAuthGuard)
+    async wargaDashboard(
+        @CurrentUser("userId") userId: number
+    ) {
+        return this.dashboardService.wargaDashboard(userId)
+    }
+
+    @Get("kepala_desa")
+    @UseGuards(JwtAuthGuard)
+    async kepalaDesaDashboard(
+        @CurrentUser("userId") userId: number
+    ) {
+        return this.dashboardService.kepalaDesaDashboard(userId)
+    }
+
+
+    @Get("rt")
+    @UseGuards(JwtAuthGuard)
+    async rtDashboard(
+        @CurrentUser("userId") userId: number
+    ) {
+        return this.dashboardService.rtDashboard(userId)
+    }
+
+    @Get("rw")
+    @UseGuards(JwtAuthGuard)
+    async rwDashboard(
+        @CurrentUser("userId") userId: number
+    ) {
+        return this.dashboardService.rwDashboard(userId)
+    }
+
 }
